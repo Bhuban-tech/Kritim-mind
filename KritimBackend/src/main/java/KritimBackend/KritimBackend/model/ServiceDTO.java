742d9fb52c9ca5ab3725ada6b795ae8de8bbc70b
karankapali <karan.kritim@gmail.com>
2025-07-09 12:43:21 +0545

    service details not showing fixed

--- KritimBackend/src/main/java/KritimBackend/KritimBackend/model/ServiceDTO.java
+++ KritimBackend/src/main/java/KritimBackend/KritimBackend/model/ServiceDTO.java
@@ -1,29 +1,39 @@
 package KritimBackend.KritimBackend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class ServiceDTO {
+    @JsonProperty("ServiceId")
     private Long ServiceId;                    // for update/edit purposes
+    @JsonProperty("ServiceName")
     private String ServiceName;
+    @JsonProperty("ServiceDescription")
     private String ServiceDescription;
 
     @JsonIgnore
     private MultipartFile ImageFile;          // used only during upload (ignored in JSON response)
-
+    @JsonProperty("ImageData")
     private byte[] ImageData;                 // actual stored image data
+
+    @JsonProperty("ImageType")
     private String ImageType;                 // e.g., "image/png"
-    private String imageBase64;               // for frontend rendering
+
+    @JsonProperty("ImageBase64")
+    private String ImageBase64;               // for frontend rendering
+
+    @JsonProperty("UserId")
     private Long UserId;
 
     @Override
     public String toString() {
         return "ServiceCatalogDto{" +
-                "serviceName='" + ServiceName + '\'' +
-                ", serviceDescription='" + ServiceDescription + '\'' +
-                ", imageFile=" + (ImageFile != null ? ImageFile.getOriginalFilename() : "null") +
+                "ServiceName='" + ServiceName + '\'' +
+                ", ServiceDescription='" + ServiceDescription + '\'' +
+                ", ImageFile=" + (ImageFile != null ? ImageFile.getOriginalFilename() : "null") +
                 '}';
     }
 }
