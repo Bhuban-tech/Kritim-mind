02807daad4121c4d06e32bf58f803b2ed632f569
Hiyanjong <hiyanjong.Kritim@gmail.com>
2025-07-10 11:29:45 +0545

    fdas

--- KritimBackend/src/main/java/KritimBackend/KritimBackend/configuration/WebConfig.java
+++ KritimBackend/src/main/java/KritimBackend/KritimBackend/configuration/WebConfig.java
@@ -1,16 +1,16 @@
-package KritimBackend.KritimBackend.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:3000")
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-    }
-};;
+//package KritimBackend.KritimBackend.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://127.0.0.1:3000")
+//                .allowCredentials(true)
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+//    }
+//};;
