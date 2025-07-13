f72af2cad0350a603fed5a41629754ff10aaf57e
himalsubedi <himal.kritim@gmail.com>
2025-07-13 19:25:14 +0545

    new

--- KritimBackend/src/main/java/KritimBackend/KritimBackend/repository/NoticeRepo.java
+++ KritimBackend/src/main/java/KritimBackend/KritimBackend/repository/NoticeRepo.java
@@ -3,4 +3,9 @@ package KritimBackend.KritimBackend.repository;
 import KritimBackend.KritimBackend.model.Notices;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoticeRepo extends JpaRepository<Notices, Long> {}
+import java.util.Optional;
+
+public interface NoticeRepo extends JpaRepository<Notices, Long> {
+    @Override
+    Optional<Notices> findById(Long aLong);
+}
--- KritimBackend/src/main/java/KritimBackend/KritimBackend/repository/TestimonialsRepo.java
+++ KritimBackend/src/main/java/KritimBackend/KritimBackend/repository/TestimonialsRepo.java
@@ -3,5 +3,9 @@ package KritimBackend.KritimBackend.repository;
 import KritimBackend.KritimBackend.model.Testimonials;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TestimonialsRepo extends JpaRepository<Testimonials, Integer> {
+    @Override
+    Optional<Testimonials> findById(Integer integer);
 }
