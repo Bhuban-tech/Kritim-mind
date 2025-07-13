c96c79055f80c30a15e26ae8e5f87d41a390589b
himalsubedi <himal.kritim@gmail.com>
2025-07-13 19:47:21 +0545

    new

--- KritimBackend/src/main/java/KritimBackend/KritimBackend/repository/NoticeRepo.java
+++ KritimBackend/src/main/java/KritimBackend/KritimBackend/repository/NoticeRepo.java
@@ -2,10 +2,18 @@ package KritimBackend.KritimBackend.repository;
 
 import KritimBackend.KritimBackend.model.Notices;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
+@Repository
 public interface NoticeRepo extends JpaRepository<Notices, Long> {
-    @Override
-    Optional<Notices> findById(Long aLong);
+
+    @Query("SELECT n FROM Notices n WHERE n.Publisher.userId = :userId")
+    List<Notices> findByPublisherId(@Param("userId") Long userId);
+
+    @Query("SELECT n FROM Notices n WHERE n.Title = :title AND n.Publisher.userId = :userId")
+    List<Notices> findByTitleAndPublisherId(@Param("title") String title, @Param("userId") Long userId);
 }
--- KritimBackend/src/main/java/KritimBackend/KritimBackend/repository/TestimonialsRepo.java
+++ KritimBackend/src/main/java/KritimBackend/KritimBackend/repository/TestimonialsRepo.java
@@ -2,10 +2,18 @@ package KritimBackend.KritimBackend.repository;
 
 import KritimBackend.KritimBackend.model.Testimonials;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
+
+@Repository
 
 public interface TestimonialsRepo extends JpaRepository<Testimonials, Integer> {
-    @Override
-    Optional<Testimonials> findById(Integer integer);
+    @Query("SELECT t FROM Testimonials t WHERE t.user.userId = :userId")
+    List<Testimonials> findByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT t FROM Testimonials t WHERE t.Name = :name AND t.user.userId = :userId")
+    List<Testimonials> findByNameAndUserId(@Param("name") String name, @Param("userId") Long userId);
 }
